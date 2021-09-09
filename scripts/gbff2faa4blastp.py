6f3a63a683099d06ca61bbe52534e9727bce5fca
19971112 <warashinatomorrow1112@gmail.com>
2021-09-09 17:12:14 +0900

    Update gbff2faa4blastp.py

--- scripts/gbff2faa4blastp.py
+++ scripts/gbff2faa4blastp.py
@@ -3,7 +3,9 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from collections import defaultdict
 
+#argvs = sys.argv
 gb_file = sys.argv[1]
+
 i = 1
 for record in SeqIO.parse(gb_file, 'genbank'):
 
@@ -29,28 +31,29 @@ for record in SeqIO.parse(gb_file, 'genbank'):
             if 'translation' in feature.qualifiers:
                 cds_seq = (feature.qualifiers['translation'][0])
 
-            #if 'product' in feature.qualifiers:
-            #    protein = (feature.qualifiers['product'][0])
-
-
+            # get product
             if 'product' in feature.qualifiers:
-                protein = feature.qualifiers['product'][0]
+                protein = feature.qualifiers['product'][0].replace(' ', '-')
             else:
                 protein = 'NA'
 
-            #for xref in feature.qualifiers['db_xref']:
-                #if xref[0:6] == 'GeneID:':
-                    #geneid = xref[6:]
+            # get organism
+            organism = record.annotations['organism']
+
+            # get gene
+            gene = feature.qualifiers.get("gene", [""])[0]
+
+            # get locus_tag
+            locus = feature.qualifiers['locus_tag'][0]
 
             #join, chenge list
             classs = record.annotations['taxonomy']
             classs1 = classs[0:3]
             classs2 = '_'.join(classs1)
-            locus = feature.qualifiers['locus_tag'][0]
 
-            organism = record.annotations['organism']
 
             #print ('>' + str(i) + '_'+ taxid + '_' + classs2 + '_' + organism + '_' + pseudo)
-            print ('>' + str(i) + '_' + protein + '_' + classs2 + '_' + organism)
+            #print ('>' + str(i) + '_' + protein + '_' + classs2 + '_' + organism)
+            print ('>' + str(i) + '_' + locus + ' ' + protein + '_' + organism)
             print (cds_seq)
             i += 1
