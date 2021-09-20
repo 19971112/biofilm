ee60152af719e522e72e9fd67a8d635f450bd1b7
19971112 <warashinatomorrow1112@gmail.com>
2021-09-20 22:07:18 +0900

    Create fasta_extract.py

---
+++ scripts/fasta_extract.py
@@ -0,0 +1,13 @@
+import sys
+from Bio import SeqIO
+
+args = sys.argv
+
+fasta_in = args[1]
+query = args[2]
+
+ for record in SeqIO.parse(fasta_in, 'fasta'):
+    if record.id == query:
+       print(">",record.description)
+       print(record.seq)
+       break
