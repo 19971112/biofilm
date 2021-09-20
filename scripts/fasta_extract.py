497795cc66f924e3839c27fc6da9634eff4085dc
19971112 <warashinatomorrow1112@gmail.com>
2021-09-20 22:55:24 +0900

    Update fasta_extract.py

--- scripts/fasta_extract.py
+++ scripts/fasta_extract.py
@@ -6,8 +6,8 @@ args = sys.argv
 fasta_in = args[1]
 query = args[2]
 
- for record in SeqIO.parse(fasta_in, 'fasta'):
-    if record.id == query:
-       print(">",record.description)
-       print(record.seq)
-       break
+for record in SeqIO.parse(fasta_in, 'fasta'):
+   if record.id == query:
+      print(">",record.description)
+      print(record.seq)
+      break
